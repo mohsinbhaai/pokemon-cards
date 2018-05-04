08d7dcf76bb317696f56ee40484df42738f734cb
Mohsin Shaikh <mohsinshaikhms0@gmail.com>
2018-05-04 09:56:27 +0530

    minor changesx

--- controllers/cards.js
+++ controllers/cards.js
@@ -21,7 +21,6 @@ exports.getCardByID = (req, res) => {
 Pokemon.findById(id).then((doc) => {
   res.send(doc);
 }, (e) => {
-//  res.status(4000).send(e);
 console.log(e);
 })
 }
@@ -52,27 +51,9 @@ exports.updateCardById = (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['name']);
 
-  // if(._isBoolean(body.completed) && body.completed) {
-  //
-  // }
   Pokemon.findByIdAndUpdate(id, {$set: body}, {new: true}).then((docs) => {
     res.send(docs);
   }, (e) => {
     res.status(4000).send(e);
   })
 }
-
-// exports.putUpdateCard = (req, res) => {
-//   Pokemon.findById(req.params.id,(err, pokemon) =>{
-//     //TODO code error statement
-//     if(pokemon)
-//     {
-//       pokemon.name = req.body.name
-//       //TODO check callback function has an error paramter or not
-//       pokemon.save((err, pokemon)=>{
-//         //TODO check err and return object
-//         res.json({message:'Pokemon card updated' })
-//       })
-//     }
-//   })
-// }
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const cardCtrl = require('./controllers/cards');
 var app = express()
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/pokemon');
-//TODO check if moongoose is connected
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
